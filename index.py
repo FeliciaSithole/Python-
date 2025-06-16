0911a71de32f7fe8ca7588a9fd1111138a752d79
Felicia Sithole <feliciasithole.fs@gmail.com>
2025-06-16 22:41:59 +0200

    adding advanced concept - strings

--- index.py
+++ index.py
@@ -1,6 +1,15 @@
 #String
 
-message = """Hellow's World"""
+#message = """Hellow's World"""
 
 
-print(message)
+#print(message)
+
+#Advanced concept - strings
+
+message = "Felicia"
+
+print(message[0])
+print(message[1])
+print(message[2])
+print(message[-1])
