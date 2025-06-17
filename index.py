bbd6b681e33bba29500c38cef31899f9823be1d1
Felicia Sithole <feliciasithole.fs@gmail.com>
2025-06-17 23:40:48 +0200

    Numeric Data

--- index.py
+++ index.py
@@ -7,11 +7,23 @@
 
 #Advanced concept - strings
 
-message = ' Felicia, Sithole '
+#message = ' Felicia, Sithole '
 
 
-print(message.strip()) #Remove leading and trailing whitespace
-print(message.lower()) #Convert all characters to lowercase
-print(message.split(',')) #Split the string into a list based on the coma
-print(message.upper()) #Convert all characters to uppercase
-print(message.replace("e" , "a")) #replaces a specified phrase with another specified phrase
+#print(message.strip()) #Remove leading and trailing whitespace
+#print(message.lower()) #Convert all characters to lowercase
+#print(message.split(',')) #Split the string into a list based on the coma
+#print(message.upper()) #Convert all characters to uppercase
+#print(message.replace("e" , "a")) #replaces a specified phrase with another specified phrase
+
+
+
+
+#Numeric Data - integer (class int) are whole numbers and floats (class float) are decimal numbers
+num = 3
+
+print(type(num))
+
+num2 = 3.14444
+
+print(type(num2))
