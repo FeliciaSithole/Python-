b7c6539d8e4f22045f0cdf99e891516bea941a18
Felicia Sithole <feliciasithole.fs@gmail.com>
2025-06-17 23:31:57 +0200

    adding upper and replace method of strings

--- index.py
+++ index.py
@@ -12,4 +12,6 @@ message = ' Felicia, Sithole '
 
 print(message.strip()) #Remove leading and trailing whitespace
 print(message.lower()) #Convert all characters to lowercase
-print(message.split()) #Split the string into a list based on the coma
+print(message.split(',')) #Split the string into a list based on the coma
+print(message.upper()) #Convert all characters to uppercase
+print(message.replace("e" , "a")) #replaces a specified phrase with another specified phrase
