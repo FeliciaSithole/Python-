c417213947bd4912a995cee37620513f529defb8
Felicia Sithole <feliciasithole.fs@gmail.com>
2025-06-18 15:15:49 +0200

    adding part 2 of Control Statements

--- index.py
+++ index.py
@@ -83,11 +83,26 @@
 # Control Statement - are fundamental components of programming language that allow us to control the flow of execution based on certain conditions 
 # if, else if and else statemet
 
-num = 0
+# num = 0
 
-if num > 0:
-    print("This number is positive")
-elif num == 0:
-    print("This number is zero")
+# if num > 0:
+    # print("This number is positive")
+# elif num == 0:
+    # print("This number is zero")
+# else:
+    # print("This number is negative")
+    
+    
+    
+    
+    
+#Control Statements
+num1 = int(input("Enter the first number:"))
+num2 = int(input("Enter the second number:"))
+
+if num1 > num2: 
+    print(num1, "is greater than", num2)
+elif num2 > num1:
+    print(num2, "is greater than", num1)
 else:
-    print("This number is negative")
+    print("Both numbers are equal")
